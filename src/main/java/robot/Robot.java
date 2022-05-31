3ab86c1208441ba6e6dd218e0f512dee4c07cf48
d.żebracki <d.zebracki@gmail.com>
2022-05-31 10:22:47 +0200

    feat: spring boot configuration to main entry point

---
+++ src/main/java/robot/Robot.java
@@ -0,0 +1,12 @@
+package robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Robot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Robot.class, args);
+    }
+}
