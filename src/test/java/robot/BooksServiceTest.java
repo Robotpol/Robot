88783bfec13c072ca86d9d54e8cfa3297d2fd090
Damian Ciepiela <dciepiela@yahoo.com>
2022-06-03 09:38:58 +0200

    test: service logic

---
+++ src/test/java/robot/BooksServiceTest.java
@@ -0,0 +1,47 @@
+package robot;
+
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+@Listeners(MockitoTestNGListener.class)
+public class BooksServiceTest {
+
+    private BooksService underTest;
+
+    @BeforeMethod
+    public void setUp() {
+        underTest = new BooksService();
+    }
+
+    @Test
+    public void shouldCacheBooks() {
+        // given
+        String title = "test";
+        String author = "test";
+        BigDecimal oldPrice = new BigDecimal(10);
+        BigDecimal newPrice = new BigDecimal(1);
+        Book book = new Book(title, author, oldPrice, newPrice);
+        Bookstore bookstore = Bookstore.BONITO;
+        // when
+        underTest.cacheBooks(bookstore, new Books(List.of(book)));
+        Books books = underTest.getBooks(bookstore.name());
+        // then
+        Book cachedBook = books.books().get(0);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(books.books().size(), 1);
+        softAssert.assertEquals(cachedBook.author(), author);
+        softAssert.assertEquals(cachedBook.title(), title);
+        softAssert.assertEquals(cachedBook.oldPrice(), oldPrice);
+        softAssert.assertEquals(cachedBook.price(), newPrice);
+        softAssert.assertAll();
+    }
+
+}
---
+++ src/test/java/robot/ScrapperServiceTest.java
@@ -0,0 +1,65 @@
+package robot;
+
+import org.mockito.Mock;
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+import java.util.Locale;
+
+@Listeners(MockitoTestNGListener.class)
+public class ScrapperServiceTest {
+
+    @Mock
+    private BooksService booksService;
+
+    private ScrapperService underTest;
+
+    @BeforeMethod
+    public void setUp() {
+        underTest = new ScrapperService(booksService);
+    }
+
+    @Test
+    public void shouldAddBonitoScrapper() {
+        // given
+        Bookstore bonito = Bookstore.BONITO;
+        int hour = 0;
+        int min = 0;
+        ScrapperPostDto scrapperPostDto = new ScrapperPostDto(bonito.name(), hour, min);
+        // when
+        underTest.startExecutionAt(scrapperPostDto, 0);
+        List<ScrapperJobDto> scrapperJobs = underTest.getScrapperJobs();
+        // then
+        ScrapperJobDto scrapperJob = scrapperJobs.get(0);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(scrapperJobs.size(), 1);
+        softAssert.assertEquals(bonito.name().toLowerCase(Locale.ROOT), scrapperJob.bookstore());
+        softAssert.assertEquals(scrapperJob.hour(), hour);
+        softAssert.assertEquals(scrapperJob.min(), min);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void shouldAddGandalfScrapper() {
+        // given
+        Bookstore bonito = Bookstore.GANDALF;
+        int hour = 0;
+        int min = 0;
+        ScrapperPostDto scrapperPostDto = new ScrapperPostDto(bonito.name(), hour, min);
+        // when
+        underTest.startExecutionAt(scrapperPostDto, 0);
+        List<ScrapperJobDto> scrapperJobs = underTest.getScrapperJobs();
+        // then
+        ScrapperJobDto scrapperJob = scrapperJobs.get(0);
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(scrapperJobs.size(), 1);
+        softAssert.assertEquals(bonito.name().toLowerCase(Locale.ROOT), scrapperJob.bookstore());
+        softAssert.assertEquals(scrapperJob.hour(), hour);
+        softAssert.assertEquals(scrapperJob.min(), min);
+        softAssert.assertAll();
+    }
+}
