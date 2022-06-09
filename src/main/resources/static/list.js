76db773e3db6e0064689cb54e8feb59cac578698
Mariusz Bal <mariusz_bal@epam.com>
2022-06-09 15:07:22 +0200

    feat: create UI

---
+++ src/main/resources/static/list.js
@@ -0,0 +1,60 @@
+var BASE_URL = "http://localhost:8080/api/v1/";
+
+var BOOKSTORE = {
+    BONITO: "bonito",
+    GANDALF: "gandalf"
+};
+
+function requestBooks(library) {
+	var req = new XMLHttpRequest();
+	req.open('GET', BASE_URL +'books/' + library, true);
+	req.onreadystatechange = function (aEvt) {
+	  if (req.readyState == 4) {
+	     if(req.status == 200)
+	      fillTable(JSON.parse(req.responseText).books);
+	     else
+	      console.error("An error occurred while fetching the books");
+	  }
+	};
+	req.send(null);
+}
+
+function fillTable(books) {
+	var table = document.getElementById('books');
+	table.innerHTML = "";
+	for (var i = 0; i < books.length; i++) {
+		let tr = document.createElement("tr");
+		tr.appendChild(createTableCell(books[i].title, 'title'));
+		tr.appendChild(createTableCell(books[i].author, 'author'));
+		tr.appendChild(createTableCell(books[i].oldPrice, 'oldPrice'));
+		tr.appendChild(createTableCell(books[i].price, 'newPrice'));
+		tr.appendChild(createLinkCell(createTableCell('', 'link'), books[i].link));
+		table.appendChild(tr);
+	}
+}
+
+function createTableCell(content, className) {
+	let td = document.createElement("td");
+	td.innerText = content;
+	td.className = className;
+	return td;
+}
+
+function createLinkCell(tableCell, link) {
+	tableCell.innerHTML = '<a href="' + link + '"><span class="material-icons">open_in_new</span></a>';
+	return tableCell;
+}
+
+function bookstoreClicked(bookstore) {
+	if (bookstore == BOOKSTORE.BONITO) {
+		document.getElementById('bonito').className = 'btn btn-primary';
+		document.getElementById('gandalf').className = 'btn btn-outline-primary';
+		requestBooks(BOOKSTORE.BONITO);
+	} else if (bookstore == BOOKSTORE.GANDALF) {
+		document.getElementById('gandalf').className = 'btn btn-primary';
+		document.getElementById('bonito').className = 'btn btn-outline-primary';
+		requestBooks(BOOKSTORE.GANDALF);
+	}
+}
+
+bookstoreClicked(BOOKSTORE.GANDALF);
