32d93fa4cb26b68c155d3a234a85f7ba5691cf0d
Damian Ciepiela <dciepiela@yahoo.com>
2022-06-10 09:33:26 +0200

    temp

--- src/main/jib/docker-entrypoint.sh
+++ src/main/jib/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-apt-get install -y firefox
+yum install -y wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum install -y ./google-chrome-stable_current_*.rpm
 
 # Assumes `java` is on PATH in the base image.
 exec java $JAVA_OPTS -cp @/app/jib-classpath-file @/app/jib-main-class-file
