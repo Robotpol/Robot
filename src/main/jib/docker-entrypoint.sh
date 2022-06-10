08bb5034b56b2fa39686082f439536201e20b55b
Damian Ciepiela <dciepiela@yahoo.com>
2022-06-10 09:42:20 +0200

    DUPA

--- src/main/jib/docker-entrypoint.sh
+++ src/main/jib/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 yum install -y wget
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-yum install -y ./google-chrome-stable_current_*.rpm
+wget -O /tmp/google-chrome-stable.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum install -y /tmp/google-chrome-stable.rpm
+
+yum install -y unzip
+wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 
 # Assumes `java` is on PATH in the base image.
 exec java $JAVA_OPTS -cp @/app/jib-classpath-file @/app/jib-main-class-file
