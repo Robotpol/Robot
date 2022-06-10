fe37a1e986b8112a796b531b4569f2707eee48cb
d.żebracki <d.zebracki@gmail.com>
2022-06-10 12:15:21 +0200

    fix: tests

--- src/main/java/robot/bonito/BonitoService.java
+++ src/main/java/robot/bonito/BonitoService.java
@@ -1,5 +1,6 @@
 package robot.bonito;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import robot.Books;
 import robot.BookstoreScrapper;
@@ -13,6 +14,7 @@ class BonitoService {
     private final BonitoBookRepository bonitoBookRepository;
     private final BookstoreScrapper scrapper;
 
+    @Autowired
     BonitoService(BonitoBookRepository bookRepository) {
         this.bonitoBookRepository = bookRepository;
         this.scrapper = new BonitoScrapper();
