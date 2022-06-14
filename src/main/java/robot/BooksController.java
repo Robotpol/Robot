f25a619bb64f9bfe7d9b7cdc325d993cba01fd71
Mariusz Bal <mariusz_bal@epam.com>
2022-06-14 16:15:29 +0200

    feat: filters retrieving in the controller

--- src/main/java/robot/BooksController.java
+++ src/main/java/robot/BooksController.java
@@ -1,10 +1,9 @@
 package robot;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/books")
@@ -18,8 +17,10 @@ public class BooksController {
 
     @GetMapping("{bookstore}")
     ResponseEntity<Books> getBooks(
-            @PathVariable String bookstore
-    ) {
+            @PathVariable String bookstore,
+            @RequestParam Map<String, String> filters
+            ) {
+        filters.forEach((f, l) -> System.out.println(f + ":" + l));
         Books books = bookstores.getBooks(bookstore);
         return ResponseEntity.ok(books);
     }
