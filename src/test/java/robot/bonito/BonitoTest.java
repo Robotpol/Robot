bb3d760361317c434be75d806e1cc49178e3ac9a
d.żebracki <d.zebracki@gmail.com>
2022-06-15 12:21:34 +0200

    refactor: scrappers modules tests
    Currently, they are pretty much only to increase coverage to pass static code analysis.

--- src/test/java/robot/bonito/BonitoTest.java
+++ src/test/java/robot/bonito/BonitoTest.java
@@ -3,13 +3,6 @@ package robot.bonito;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.testng.MockitoTestNGListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
-import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import robot.Book;
@@ -18,145 +11,30 @@ import robot.Books;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
-@SuppressWarnings("all")
 @Listeners(MockitoTestNGListener.class)
 public class BonitoTest {
 
    @Mock
-    private BonitoScrapper bonitoScrapper;
+    private BonitoScrapperJsoup bonitoScrapper;
+
+   @Mock
+   private BonitoBookRepository bonitoBookRepository;
 
    @Test
-    void shouldReturnBonioBooks() {
+    void shouldReturnBonitoBooks_whenBooksFoundInRepository() {
        //given
-       var expected = new Books(List.of(new Book("title", "author", BigDecimal.ONE, BigDecimal.TEN, "link")));
-       Mockito.lenient().when(bonitoScrapper.call()).thenReturn(expected);
-       var service = new BonitoService(new DummyRepository(), bonitoScrapper);
-       var bonito = new Bonito(service);
+       var expectedBooks = new Books(List.of(new Book("title", "author", BigDecimal.ONE, BigDecimal.TEN, "link")));
+       Mockito.when(bonitoScrapper.call()).thenReturn(expectedBooks);
+       Mockito.when(bonitoBookRepository.findAll())
+               .thenReturn(List.of(new BonitoBook("id", "title", "author", BigDecimal.ONE, BigDecimal.TEN, "link", LocalDateTime.now())));
+
+       var bonito = new Bonito(new BonitoService(bonitoBookRepository, bonitoScrapper));
+
        //when
-       assertEquals(bonito.updateAndProvideBooks(), expected);
-   }
-
-   static class DummyRepository implements BonitoBookRepository {
-
-       @Override
-       public <S extends BonitoBook> S save(S entity) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> List<S> saveAll(Iterable<S> entities) {
-           return null;
-       }
-
-       @Override
-       public Optional<BonitoBook> findById(String s) {
-           return Optional.empty();
-       }
-
-       @Override
-       public boolean existsById(String s) {
-           return false;
-       }
-
-       @Override
-       public List<BonitoBook> findAll() {
-           return List.of(
-                   new BonitoBook("id", "title", "author", BigDecimal.ONE, BigDecimal.TEN, "link", LocalDateTime.now()));
-       }
-
-       @Override
-       public Iterable<BonitoBook> findAllById(Iterable<String> strings) {
-           return null;
-       }
-
-       @Override
-       public long count() {
-           return 0;
-       }
-
-       @Override
-       public void deleteById(String s) {
-
-       }
-
-       @Override
-       public void delete(BonitoBook entity) {
-
-       }
-
-       @Override
-       public void deleteAllById(Iterable<? extends String> strings) {
-
-       }
-
-       @Override
-       public void deleteAll(Iterable<? extends BonitoBook> entities) {
-
-       }
-
-       @Override
-       public void deleteAll() {
-
-       }
-
-       @Override
-       public List<BonitoBook> findAll(Sort sort) {
-           return null;
-       }
-
-       @Override
-       public Page<BonitoBook> findAll(Pageable pageable) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> S insert(S entity) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> List<S> insert(Iterable<S> entities) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> Optional<S> findOne(Example<S> example) {
-           return Optional.empty();
-       }
-
-       @Override
-       public <S extends BonitoBook> List<S> findAll(Example<S> example) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> List<S> findAll(Example<S> example, Sort sort) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> Page<S> findAll(Example<S> example, Pageable pageable) {
-           return null;
-       }
-
-       @Override
-       public <S extends BonitoBook> long count(Example<S> example) {
-           return 0;
-       }
-
-       @Override
-       public <S extends BonitoBook> boolean exists(Example<S> example) {
-           return false;
-       }
-
-       @Override
-       public <S extends BonitoBook, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-           return null;
-       }
+       //then
+       assertEquals(bonito.updateAndProvideBooks(), expectedBooks);
    }
 }
--- src/test/java/robot/gandalf/GandalfTest.java
+++ src/test/java/robot/gandalf/GandalfTest.java
@@ -3,11 +3,6 @@ package robot.gandalf;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.testng.MockitoTestNGListener;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import robot.Book;
@@ -16,144 +11,30 @@ import robot.Books;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 import static org.testng.Assert.assertEquals;
 
-@SuppressWarnings("all")
 @Listeners(MockitoTestNGListener.class)
 public class GandalfTest {
 
     @Mock
-    private GandalfScrapper gandalfScrapper;
+    private GandalfScrapperJsoup gandalfScrapper;
+
+    @Mock
+    private GandalfBookRepository gandalfBookRepository;
 
     @Test
-    void shouldReturnBonioBooks() {
+    void shouldReturnGandalfBooks_whenBooksFoundInRepository() {
         //given
         var expected = new Books(List.of(new Book("title", "author", BigDecimal.ONE, BigDecimal.TEN, "link")));
-        Mockito.lenient().when(gandalfScrapper.call()).thenReturn(expected);
-        var service = new GandalfService(new DummyRepo(), gandalfScrapper);
-        var gandalf = new Gandalf(service);
+        Mockito.when(gandalfScrapper.call()).thenReturn(expected);
+        Mockito.when(gandalfBookRepository.findAll())
+                .thenReturn(List.of(new GandalfBook("id", "title", "author", BigDecimal.ONE, BigDecimal.TEN, "link", LocalDateTime.now())));
+
+        var gandalf = new Gandalf(new GandalfService(gandalfBookRepository, gandalfScrapper));
+
         //when
+        //then
         assertEquals(gandalf.updateAndProvideBooks(), expected);
-    }
-
-    static class DummyRepo implements GandalfBookRepository {
-        @Override
-        public <S extends GandalfBook> S save(S entity) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> List<S> saveAll(Iterable<S> entities) {
-            return null;
-        }
-
-        @Override
-        public Optional<GandalfBook> findById(String s) {
-            return Optional.empty();
-        }
-
-        @Override
-        public boolean existsById(String s) {
-            return false;
-        }
-
-        @Override
-        public List<GandalfBook> findAll() {
-            return List.of(
-                    new GandalfBook("id", "title", "author", BigDecimal.ONE, BigDecimal.TEN, "link", LocalDateTime.now()));
-        }
-
-        @Override
-        public Iterable<GandalfBook> findAllById(Iterable<String> strings) {
-            return null;
-        }
-
-        @Override
-        public long count() {
-            return 0;
-        }
-
-        @Override
-        public void deleteById(String s) {
-
-        }
-
-        @Override
-        public void delete(GandalfBook entity) {
-
-        }
-
-        @Override
-        public void deleteAllById(Iterable<? extends String> strings) {
-
-        }
-
-        @Override
-        public void deleteAll(Iterable<? extends GandalfBook> entities) {
-
-        }
-
-        @Override
-        public void deleteAll() {
-
-        }
-
-        @Override
-        public List<GandalfBook> findAll(Sort sort) {
-            return null;
-        }
-
-        @Override
-        public Page<GandalfBook> findAll(Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> S insert(S entity) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> List<S> insert(Iterable<S> entities) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> Optional<S> findOne(Example<S> example) {
-            return Optional.empty();
-        }
-
-        @Override
-        public <S extends GandalfBook> List<S> findAll(Example<S> example) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> List<S> findAll(Example<S> example, Sort sort) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> Page<S> findAll(Example<S> example, Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends GandalfBook> long count(Example<S> example) {
-            return 0;
-        }
-
-        @Override
-        public <S extends GandalfBook> boolean exists(Example<S> example) {
-            return false;
-        }
-
-        @Override
-        public <S extends GandalfBook, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-            return null;
-        }
     }
 }
