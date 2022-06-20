75df6eaa364f487ea46c1315c7993912b34eb9aa
Damian Ciepiela <dciepiela@yahoo.com>
2022-06-20 10:59:22 +0200

    build: remove jsoup fix: imports (#68)

--- src/main/java/robot/BookstoreScrapper.java
+++ src/main/java/robot/BookstoreScrapper.java
@@ -2,8 +2,6 @@ package robot;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -25,21 +23,6 @@ interface BookstoreScrapper extends Callable<Books> {
     int findPageCount(Document document);
 
     Book readBookInfo(Element book);
-
-    default void waitForPageLoad(WebDriver driver) {
-        while (true) {
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            String result = js.executeScript("return document.readyState").toString();
-            if (result.equals("complete")) {
-                break;
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
 
     default BigDecimal transformPrice(String price) {
         return new BigDecimal(replaceCommaInPrice(trimZl(price)));
